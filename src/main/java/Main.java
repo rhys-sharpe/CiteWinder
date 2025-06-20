0a407225e3de007c85a4a675e738f50e82036e22
Rhys Sharpe <davidrsharpe@southern.edu>
2025-06-20 09:36:25 -0400

    starter code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import org.jbibtex.ParseException;
 
 public class Main {
+    public static void main(String[] args) throws ParseException {
+        System.out.println("Welcome - BibTeX Manager 0.1");
+        org.jbibtex.BibTeXParser parser = new org.jbibtex.BibTeXParser();
+    }
 }
